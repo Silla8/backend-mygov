417e4fd4ce4b286bf1d746876ebceb9b8374b753
silla <ibsilla2@gmail.com>
2024-09-03 02:23:27 +0400

    g

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const Pool= require("pg").Pool;
 const pool = new Pool({
-    user: "postgres",
-    password: "silla",
-    host: "localhost",
+    user: "mygov_user",
+    password: "cSuuFm22zTit1PxQLj5DD8C4H1lY3nWG",
+    host: "dpg-crb3efjtq21c73cf8adg-a.frankfurt-postgres.render.com",
     port: 5432,
     database: "mygov"
 });
